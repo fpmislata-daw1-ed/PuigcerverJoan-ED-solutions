a1f04157cc4ff4ffed0c824b76e1b103675cae0a
Joan Puigcerver <joapuiib@gmail.com>
2022-11-02 19:16:27 +0100

    Added functionality: Ask N to the user

--- src/main/java/ud2/exercises/MultiplyTableFull.java
+++ src/main/java/ud2/exercises/MultiplyTableFull.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 public class MultiplyTableFull {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
+        int n = in.nextInt();
 
         for (int i = 1; i <= 10; i++) {
             for (int j = 5; j <= 20; j++) {
